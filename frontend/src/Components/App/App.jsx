1dba563604e71d487b4854577e834c8bbb3a9557
Natalie Kinsaul <n.kinsaul@gmail.com>
2024-02-20 12:22:39 -0800

    Create empty GameGrid Component

--- frontend/src/Components/App/App.jsx
+++ frontend/src/Components/App/App.jsx
@@ -1,10 +1,14 @@
+//import styling
 import './App.css';
+//import Components
 import Header from '../Header/Header';
+import GameGrid from '../GameGrid/GameGrid';
 
 const App = () => {
   return (
     <main className='App'>
       <Header />
+      <GameGrid />
     </main>
   )
 }
---
+++ frontend/src/Components/GameGrid/GameGrid.jsx
@@ -0,0 +1,12 @@
+//import styling
+import './GameGrid.css';
+
+const GameGrid = () => {
+  return (
+    <div>
+      <p>GameGrid</p>
+    </div>
+  )
+}
+
+export default GameGrid;
